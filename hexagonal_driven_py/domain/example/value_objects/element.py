f43076dfc9f4b770983351b100aad486fdae1788
Jean Ribeiro Lustosa <jean@useorigin.com>
2021-09-16 19:52:19 -0300

    Adding Element value object.

---
+++ hexagonal_driven_py/domain/example/value_objects/element.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+from hexagonal_driven_py.domain.example.value_objects.element_enum import ElementEnum
+
+
+@dataclass
+class Element:
+    value: str
+
+    def __post_init__(self):
+        if self.value and not hasattr(ElementEnum, self.value):
+            raise Exception(f'Invalid Pokemón element: {self.value}.')
---
+++ hexagonal_driven_py/domain/example/value_objects/element_enum.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class ElementEnum(Enum):
+    STEEL = 'STEEL'
+    FIRE = 'FIRE'
+    WATER = 'WATER'
+    GRASS = 'GRASS'
