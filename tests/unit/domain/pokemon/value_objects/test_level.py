4385487defdb2f651128a35e92272ec80639122d
Gláuber Brennon <glauberbrennon@gmail.com>
2021-09-16 19:42:13 -0300

    Implemented ability and level VOsw

---
+++ tests/unit/domain/pokemon/value_objects/test_level.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+
+class TestLevel(TestCase):
+    def test_construction_WHEN_level_value_below_0_THEN_raise_validation_error(self):
+        pass
+
+    def test_construction_WHEN_level_value_above_0_THEN_returns_level_with_given_value(
+        self,
+    ):
+        pass
