c12db9ea99edbcd228337b07998063a5460c1696
Lucas Martins <lucasmartins@Lucass-MacBook-Pro.local>
2021-09-16 19:52:27 -0300

    implements damage VO

--- hexagonal_driven_py/domain/pokemon/value_objects/damage.py
+++ hexagonal_driven_py/domain/pokemon/value_objects/damage.py
@@ -5,10 +5,10 @@ from hexagonal_driven_py.domain.pokemon.exceptions.validation_error import \
 
 
 @dataclass(frozen=True)
-class Level:
-    __minimum_value_threshold = 1
+class Damage:
+    __minimum_value_threshold = 0
     value: int
 
     def __post_init__(self):
         if self.value < self.__minimum_value_threshold:
-            raise ValidationError(f'Level below {self.__minimum_value_threshold}')
+            raise ValidationError(f'Damage below {self.__minimum_value_threshold}')
